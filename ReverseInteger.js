d87a2ea338af7e877460efbd29befb90318830cb
zyabxwcd <zyabxwcd@gmail.com>
2019-01-18 18:10:18 +0530

    changed var name

--- ReverseInteger.js
+++ ReverseInteger.js
@@ -6,13 +6,13 @@ var reverse = function(x) {
     var MAX_INT = 2**31 -1; // Max input size is a 32 bit integer
     var MIN_INT = -MAX_INT;
     if (x < 0) {
-        reversed = Number(String(x*-1).split('').reverse().join(''))*-1;
+        rev = Number(String(x*-1).split('').reverse().join(''))*-1;
     } else {
-        reversed = Number(String(x).split('').reverse().join(''));
+        rev = Number(String(x).split('').reverse().join(''));
     }
-    if (reversed > MAX_INT || reversed < MIN_INT) {
+    if (rev > MAX_INT || rev < MIN_INT) {
         return 0;
     } else {
-      return reversed;  
+      return rev;  
     }
 };
