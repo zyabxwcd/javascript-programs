18a363ce88456d0ae1cb94a154a5eb5e25f4de5a
zyabxwcd <zyabxwcd@gmail.com>
2019-01-29 10:33:28 +0530

    added Bracket Balancing program

---
+++ ces-programs/BracketBalancing.py
@@ -0,0 +1,26 @@
+from pythonds.basic.stack import Stack
+
+def parChecker(string):
+    bracket = ('(','{','[')
+    s = Stack()
+    balanced = True
+    index = 0
+    while index < len(string) and balanced:
+        symbol = string[index]
+        if symbol in bracket:
+            s.push(symbol)
+        else:
+            if s.isEmpty():
+                balanced = False
+            else:
+                s.pop()
+
+        index = index + 1
+
+    if balanced and s.isEmpty():
+        return True
+    else:
+        return False
+
+string = input('Enter string: ')
+print(parChecker(string))
