8c14da8134a072dc17338e02aeff9d5760391d04
zyabxwcd <zyabxwcd@gmail.com>
2019-01-29 22:02:24 +0530

    minor modification

--- ces-programs/FindSubarrays.py
+++ ces-programs/FindSubarrays.py
@@ -10,25 +10,17 @@ def sub_arrays(array, inc=True):
     if inc:
         while j < length:
             if array[j] < array[j-1]:
-                if len(array[i:j]) == 1:
-                    i = j
-                    j = i + 1
-                else:
-                    print(array[i:j])
-                    i = j
-                    j = i + 1
+                print( array[i:j] if len( array[i:j] ) != 1 else '', end='')
+                i = j
+                j = i + 1
             else:
                 j += 1
     else:
         while j < length:
             if array[j] > array[j-1]:
-                if len(array[i:j]) == 1:
-                    i = j
-                    j = i + 1
-                else:
-                    print(array[i:j])
-                    i = j
-                    j = i + 1
+                print( array[i:j] if len( array[i:j] ) != 1 else '', end='')
+                i = j
+                j = i + 1
             else:
                 j += 1
 
@@ -36,4 +28,4 @@ def sub_arrays(array, inc=True):
 
 # Input list of numbers
 array = [int(i) for i in input().split()]
-sub_arrays(array, inc=False)
+sub_arrays(array)
