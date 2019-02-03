488b9d33fd314c6b48bd4c5e7969367c357d5bbb
zyabxwcd <zyabxwcd@gmail.com>
2019-02-03 16:37:34 +0530

    minor changes

--- ces-programs/BracketBalancing.py
+++ ces-programs/BracketBalancing.py
@@ -1,23 +1,21 @@
-from pythonds.basic.stack import Stack
-
 def bracketChecker(string):
     bracket = ('(','{','[')
-    s = Stack()
+    s = []
     balanced = True
     index = 0
     while index < len(string) and balanced:
         symbol = string[index]
         if symbol in bracket:
-            s.push(symbol)
+            s.append(symbol)
         else:
-            if s.isEmpty():
+            if len(s) == 0:
                 balanced = False
             else:
                 s.pop()
 
         index = index + 1
 
-    if balanced and s.isEmpty():
+    if balanced and len(s) == 0:
         return True
     else:
         return False
