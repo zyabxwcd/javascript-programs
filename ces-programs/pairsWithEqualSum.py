f3241e852910d811cbaefe034a42ca32c17262a1
zyabxwcd <zyabxwcd@gmail.com>
2019-03-04 17:51:53 +0530

    minor change, no logic changed

--- ces-programs/pairsWithEqualSum.py
+++ ces-programs/pairsWithEqualSum.py
@@ -3,12 +3,12 @@ Given an array of numbers. Print all unique pairs with equal sum.
 
 Output of the code below:
 1 2 3 1 4
-Pairs having a sum of 2: [(1, 1)]
-Pairs having a sum of 3: [(1, 2), (2, 1)]
-Pairs having a sum of 4: [(1, 3), (3, 1)]
-Pairs having a sum of 5: [(1, 4), (2, 3)]
-Pairs having a sum of 6: [(2, 4)]
-Pairs having a sum of 7: [(3, 4)]
+Pair(s) having a sum of 2: [(1, 1)]
+Pair(s) having a sum of 3: [(1, 2), (2, 1)]
+Pair(s) having a sum of 4: [(1, 3), (3, 1)]
+Pair(s) having a sum of 5: [(1, 4), (2, 3)]
+Pair(s) having a sum of 6: [(2, 4)]
+Pair(s) having a sum of 7: [(3, 4)]
 '''
 
 from itertools import combinations
@@ -22,5 +22,5 @@ pair_sum = {pair: sum(pair) for pair in combinations(array, 2)}
 sum_set = set(pair_sum.values())
 
 for sum in sum_set:
-    print(f'Pairs having a sum of {sum}:', 
+    print(f'Pair(s) having a sum of {sum}:', 
         [pair for pair, value in pair_sum.items() if value == sum])
